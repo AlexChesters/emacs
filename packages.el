cb959acfe755223be7af6a626e98a399ae750744
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-05-04 23:32:47 +0100

    add js2-mode

--- packages.el
+++ packages.el
@@ -15,5 +15,15 @@
   :config
   (add-hook 'after-init-hook #'global-flycheck-mode))
 
+(use-package js2-mode
+  :mode ("\\.js$" . js2-mode)
+  :config
+  (add-hook 'js2-mode-hook
+          (defun my-js2-mode-setup ()
+            (flycheck-mode t)
+            (when (executable-find "eslint")
+              (flycheck-select-checker 'javascript-eslint))))
+  (add-hook 'js2-mode-hook #'add-node-modules-path))
+
 (provide 'packages.el)
 ;;; packages.el ends here
