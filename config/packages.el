fa56433fc9d9b378c2f179eaf418056cac791ed5
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-05-07 16:16:27 +0100

    add markdown mode

--- config/packages.el
+++ config/packages.el
@@ -46,6 +46,15 @@
   (add-hook 'js2-mode-hook #'add-node-modules-path)
   (js2-mode-hide-warnings-and-errors))
 
+;; markdown-mode
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+
 ;; Projectile
 (use-package projectile
   :config
