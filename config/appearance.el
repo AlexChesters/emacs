82675f7c8dd37e36b79f56015120f2517b666570
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-05-14 11:23:15 +0100

    use display column indicator mode

--- config/appearance.el
+++ config/appearance.el
@@ -6,6 +6,8 @@
 
 (set-frame-font "Fira Code 16")
 (load-theme 'apropospriate-dark t)
+(setq display-fill-column-indicator-column 80)
+(global-display-fill-column-indicator-mode)
 
 (provide 'appearance.el)
 ;;; appearance.el ends here
