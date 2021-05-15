8bc323a4e439cd1f10ef6355487a548ef9cbb68b
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-05-15 23:28:23 +0100

    move some config

--- config/appearance.el
+++ config/appearance.el
@@ -10,6 +10,9 @@
 (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
 (add-hook 'markdown-mode-hook 'display-fill-column-indicator-mode)
 (add-hook 'prog-mode-hook 'linum-mode)
+(setq inhibit-splash-screen t) ; Disable splash screen
+(global-visual-line-mode t) ; Turning on visual line mode
+(tool-bar-mode -1) ; Hiding toolbar
 
 ;; https://github.com/AdamNiederer/vue-mode/issues/112
 (add-hook 'vue-mode-hook 'display-fill-column-indicator-mode)
--- config/misc.el
+++ config/misc.el
@@ -10,12 +10,9 @@
 (setq indent-line-function 'insert-tab)
 (setq lisp-indent-offset 2) ; Setting the indent level for lisp
 
-(setq inhibit-splash-screen t) ; Disable splash screen
-(global-visual-line-mode t) ; Turning on visual line mode
 (delete-selection-mode 1) ; Deleting selected text if typed in/pasted
 (setq make-backup-files nil) ; Preventing backup (~) files
 (fset 'yes-or-no-p 'y-or-n-p) ; Use y or n instead of yes or no
-(tool-bar-mode -1) ; Hiding toolbar
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1)) ; Hide scroll bar
 (show-paren-mode 1) ; Always show matching parenthesis
 (delete-selection-mode 1) ; Deleting selected text if typed in/pasted
