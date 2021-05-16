39cb0a6fd89bfb61395ec1c769b720057d3a8e32
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-05-16 17:48:16 +0100

    set typescript indent level

--- config/misc.el
+++ config/misc.el
@@ -9,6 +9,7 @@
 (setq-default indent-tabs-mode nil) ; Use spaces over tabs
 (setq indent-line-function 'insert-tab)
 (setq lisp-indent-offset 2) ; Setting the indent level for lisp
+(setq-default typescript-indent-level 2) ; typescript indent level
 
 (delete-selection-mode 1) ; Deleting selected text if typed in/pasted
 (setq make-backup-files nil) ; Preventing backup (~) files
