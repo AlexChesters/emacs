95ba38aecb087a47b41441b5b6e0b6a7a4b362e7
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-05-25 14:29:29 +0100

    ensure flycheck looks in node_modules for eslint executable

--- config/packages.el
+++ config/packages.el
@@ -45,6 +45,19 @@
 (use-package flycheck
   :config
   (add-hook 'after-init-hook #'global-flycheck-mode))
+(defun my/use-eslint-from-node-modules ()
+  "Search node_modules/ for eslint executable [https://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable]."
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint
+          (and root
+               (expand-file-name "node_modules/.bin/eslint"
+                                 root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
 ;; git-timemachine
 (use-package git-timemachine)
