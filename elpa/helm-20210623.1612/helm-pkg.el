0637cac4d4f051a09c1defb9e8998cada5ce8f78
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-06-24 09:46:47 +0100

    update melpa packages

--- elpa/helm-20210623.1612/helm-pkg.el
+++ elpa/helm-20210623.1612/helm-pkg.el
@@ -1,9 +1,9 @@
-(define-package "helm" "20210617.420" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20210623.1612" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "25.1")
     (async "1.9.4")
     (popup "0.5.3")
     (helm-core "3.7.1"))
-  :commit "1cfbe908426e8d62b532079c835f7bdeced92cc0" :authors
+  :commit "28e89f5dcc29d8254b59d2e3ee1fc23312e6ca9a" :authors
   '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
   :maintainer
   '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
