dd6e0a3f49f0cb969f6b607454904b1436f79c8a
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-06-28 14:37:14 +0100

    update melpa packages

--- elpa/helm-20210625.529/helm-pkg.el
+++ elpa/helm-20210625.529/helm-pkg.el
@@ -1,9 +1,9 @@
-(define-package "helm" "20210623.1612" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20210625.529" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "25.1")
     (async "1.9.4")
     (popup "0.5.3")
-    (helm-core "3.7.1"))
-  :commit "28e89f5dcc29d8254b59d2e3ee1fc23312e6ca9a" :authors
+    (helm-core "3.8.0"))
+  :commit "eaf5b01f9c0c70dca2f665b1da1b7a461dc1d112" :authors
   '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
   :maintainer
   '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
--- elpa/magit-20210628.923/magit-pkg.el
+++ elpa/magit-20210628.923/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20210620.435" "A Git porcelain inside Emacs."
+(define-package "magit" "20210628.923" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (dash "20210330")
     (git-commit "20210525")
     (magit-section "20210525")
     (transient "20210524")
     (with-editor "20210524"))
-  :commit "f592e367d7602106d6f8f8349148bd31a94ba231" :authors
+  :commit "3a39338a4382de13972adb55c528a8ec68e7ab21" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com")
     ("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
