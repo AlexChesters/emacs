21008386daa17d8c8082d17cfc063beb3a9a844f
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-07-21 10:15:34 +0100

    update melpa packages

--- elpa/magit-20210720.1413/magit-pkg.el
+++ elpa/magit-20210720.1413/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20210716.1440" "A Git porcelain inside Emacs."
+(define-package "magit" "20210720.1413" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (dash "20210330")
     (git-commit "20210701")
     (magit-section "20210701")
     (transient "20210701")
     (with-editor "20210524"))
-  :commit "bf9cc19a3287f6a3cc5c1836312f0d18b38b6e45" :authors
+  :commit "da5ac05e09b93f4d00dc6b6761860a4ca32ebfa9" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com")
     ("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
