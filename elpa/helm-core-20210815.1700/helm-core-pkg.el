7e1594862a56970c7316434663aeeb43751af864
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-08-24 11:11:32 +0100

    update melpa packages

--- elpa/helm-core-20210815.1700/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20210815.1700" "Development files for Helm"
-  '((emacs "25.1")
-    (async "1.9.4"))
-  :commit "3f89dfcb35d7a6a63647887aaf770df9a2794acb")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
