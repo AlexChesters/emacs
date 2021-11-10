69394524c444127322f7bc7cb1a41a60851867c2
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-11-10 13:52:46 +0000

    update melpa packages

--- elpa/apropospriate-theme-20211026.1722/apropospriate-theme-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "apropospriate-theme" "20211026.1722" "A colorful, low-contrast, light & dark theme set for Emacs with a fun name." 'nil :commit "2b042f49649f9361d1197355f581166a30e22e37")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
---
+++ elpa/apropospriate-theme-20211109.2307/apropospriate-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "apropospriate-theme" "20211109.2307" "A colorful, low-contrast, light & dark theme set for Emacs with a fun name." 'nil :commit "c32f8ad7a0178b1aca0af7b15ef7389aa250f154")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
