503ebb68ba7cb4e1b14f6600069779918d2ee4d0
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-11-16 10:47:23 +0000

    update melpa packages

---
+++ elpa/apropospriate-theme-20211113.1913/apropospriate-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "apropospriate-theme" "20211113.1913" "A colorful, low-contrast, light & dark theme set for Emacs with a fun name." 'nil :commit "b934a5a17cac02137c1bfe81935638ab85dbaec9")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-core-20211114.1507/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20211114.1507" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "a4786102aaa6db09ce9f5bf9aa89fe4f0fadb8fd")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
