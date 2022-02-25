2c5e4a443b4082e43af22b2bc96cc376b741439c
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-02-25 13:16:55 +0000

    update melpa packages

---
+++ elpa/helm-20220225.727/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20220225.727" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.8.4")
+    (popup "0.5.3"))
+  :commit "fbe5eb03255c18466162253c60db7b6ca50f32b4" :authors
+  '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
+  :maintainer
+  '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
