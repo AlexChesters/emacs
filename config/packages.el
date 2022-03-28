809b7be616a0ec56bfb45991c3b109e09fb6074f
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-03-28 15:22:38 +0100

    add support for tsx files

--- config/packages.el
+++ config/packages.el
@@ -233,6 +233,16 @@
 (add-hook 'typescript-mode-hook #'setup-tide)
 (add-hook 'typescript-mode-hook #'add-node-modules-path)
 
+;; tsx
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
 ;; treemacs
 (use-package treemacs
   :config
