cbc18b147b6bc421e3e688692aa0071d523b6809
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-05-11 16:45:36 +0100

    update melpa packages

--- elpa/eglot-20220427.1148/eglot-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from eglot.el  -*- no-byte-compile: t -*-
-(define-package "eglot" "20220427.1148" "Client for Language Server Protocol (LSP) servers" '((emacs "26.1") (jsonrpc "1.0.14") (flymake "1.2.1") (project "0.3.0") (xref "1.0.1") (eldoc "1.11.0") (seq "2.23")) :commit "7f1f09347214ebb6e0395f3e27a04f87f544ffdd" :authors '(("João Távora" . "joaotavora@gmail.com")) :maintainer '("João Távora" . "joaotavora@gmail.com") :keywords '("convenience" "languages") :url "https://github.com/joaotavora/eglot")
