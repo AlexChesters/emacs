24b0070fa9fc50b87f5ab51db3fc14f69d2a5069
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-05-16 08:47:03 +0100

    update melpa packages

---
+++ elpa/helm-20220513.1002/helm-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm" "20220513.1002" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.8.4")
+    (popup "0.5.3"))
+  :commit "4489fec779eb4c2f54db06218c83d431a88716b5" :authors
+  '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
+  :maintainer
+  '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-core-20220514.725/helm-core-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm-core" "20220514.725" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "4489fec779eb4c2f54db06218c83d431a88716b5" :authors
+  '(("Thierry Volpiatto" . "thierry.volpiatto@gmail.com"))
+  :maintainer
+  '("Thierry Volpiatto" . "thierry.volpiatto@gmail.com")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
