3aa586868c6ac5c039c19bb110281a44ded9284e
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-12-01 08:44:54 -0800

    update melpa packages

---
+++ elpa/project-0.9.0/project-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from project.el  -*- no-byte-compile: t -*-
+(define-package "project" "0.9.0" "Operations on the current project" '((emacs "26.1") (xref "1.4.0")) :commit "785fa801596ad7bb9f838cac865f00de29e253d1" :url "https://elpa.gnu.org/packages/project.html")
---
+++ elpa/transient-20221130.1900/transient-pkg.el
@@ -0,0 +1,13 @@
+(define-package "transient" "20221130.1900" "Transient commands"
+  '((emacs "25.1")
+    (compat "28.1.1.0"))
+  :commit "72d30284ab75c1497e23be310dcc53a4ecefa401" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("extensions")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
